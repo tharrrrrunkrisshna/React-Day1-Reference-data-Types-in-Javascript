dfd67500337cd3516a4adceb100f240fda4100e8
tharrrrrunkrisshna <43927016+tharrrrrunkrisshna@users.noreply.github.com>
2023-03-17 11:28:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+function Test()
+{
+    let myobject={name:"Madras"};
+    let newObject = myobject;
+    myobject.name="Chennai"
+    let myarray =["a","e","i","o","u"]
+    let vowelArray = myarray
+    myarray.push("u")
+    console.log(myarray)
+    const ECE = ()=>{alert("Check the console output!")}
+    return <div><button onClick={ECE}>Reference data types and call the function</button></div>
+
+}
+ReactDOM.render(<Test/>,document.getElementById('mydiv'))
